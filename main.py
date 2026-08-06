7b96dc8292f33ace5535cfa4b4172cd165959019
surakani rahul <surakanirahul@gmail.com>
2026-08-06 19:56:18 +0530

    project overview and requirements

---
+++ main.py
@@ -0,0 +1,8 @@
+# ==================================================
+# PREPTRACK - BOILERPLACE CODE# Complete every section marked TODO
+# ==================================================
+
+print("=" * 50)
+print("              PREPTRACK APPLICATION")
+print("=" * 50)
+
